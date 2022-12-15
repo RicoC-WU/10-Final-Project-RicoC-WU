88c27c4a2d350cdfb262e2c866566b36bc7dd8b5
RicoC-WU <rjcoleman@wustl.edu>
2022-12-14 21:37:50 -0600

    update

--- src/SearchTools.js
+++ src/SearchTools.js
@@ -7,16 +7,26 @@ class SearchTools extends Component{
             <button id="GoBtn" onClick={this.props.genQuotes}>GO!</button><button id="RandomBtn" onClick={this.props.genQuotes}>Get Random Quote(s)</button><br/>
             <p>Search By:</p>
             <form> 
-                Search Type: <input type="radio" id="select1" name="SearchType" value="Character" onClick={this.props.handleButtonChange}/>
-                <label htmlFor="select1">Character</label>
-                <input type="radio" id="select2" name="SearchType" value="Title" onClick={this.props.handleButtonChange}/> 
-                <label htmlFor="select2">Title</label>
+                Search Type: &nbsp;
+                <label htmlFor="select1" class="container">Character
+                    <input type="radio" id="select1" name="SearchType" value="Character" onClick={this.props.handleButtonChange}/>
+                    <span class="checkmark"></span>
+                </label>
+                <label htmlFor="select2" class="container">Title
+                    <input type="radio" id="select2" name="SearchType" value="Title" onClick={this.props.handleButtonChange}/> 
+                    <span class="checkmark"></span>
+                </label>
             </form>
             <form> 
-                Quote Amount: <input type="radio" id="select1" name="AmountType" value="1" onClick={this.props.handleAmountChange}/>
-                <label htmlFor="select1">1</label>
-                <input type="radio" id="select2" name="AmountType" value="10" onClick={this.props.handleAmountChange}/> 
-                <label htmlFor="select2">10</label>
+                &nbsp;Quote Amount: &nbsp;
+                <label htmlFor="select3" class="container">1
+                    <input type="radio" id="select3" name="AmountType" value="1" onClick={this.props.handleAmountChange}/>
+                    <span class="checkmark"></span>
+                </label>
+                <label htmlFor="select4" class="container">10
+                    <input type="radio" id="select4" name="AmountType" value="10" onClick={this.props.handleAmountChange}/> 
+                    <span class="checkmark"></span>
+                </label>
             </form>
             </div>
         );
